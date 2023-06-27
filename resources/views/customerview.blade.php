e94fb19960e5ef2c196294daeff42c9019fe5d51
Subesh Pandey <pandeypaul137@gmail.com>
2023-06-27 22:13:36 +0545

    Profile view page work

--- resources/views/customerview.blade.php
+++ resources/views/customerview.blade.php
@@ -12,235 +12,174 @@
     <title>view profile</title>
 </head>
 <style>
-    @media (max-width: 992px) {
-        .rowww {
-            margin-top: 150px;
-        }
+    /* Container styles */
+    .container.emp-profile {
+        margin-top: 95px;
+        display: flex;
+        justify-content: center;
     }
 
-    @media (min-width: 992px) {
-        .rowww {
-            margin-top: 210px;
-        }
-
+    /* Profile card styles */
+    .profile-card {
+        background-color: #fff;
+        border-radius: 10px;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+        margin: 20px;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
 
-    @media (max-width: 400px) {
-        .rowww {
-            margin-top: 78px;
-        }
-    }
-
-    body {
-        background: -webkit-linear-gradient(left, #0c00ad, #360093);
-    }
-
-    .emp-profile {
-        padding: 3%;
-        margin-top: 3%;
-        margin-bottom: 3%;
-        border-radius: 0.5rem;
-        background: #fff;
-    }
-
+    /* Profile image styles */
     .profile-img {
         text-align: center;
+        margin-bottom: 20px;
     }
 
     .profile-img img {
-        width: 70%;
-        height: 100%;
+        width: 200px;
+        height: 200px;
+        object-fit: cover;
+        border-radius: 50%;
+        border: 5px solid #fff;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
     }
 
-    .profile-img .file {
-        position: relative;
-        overflow: hidden;
-        margin-top: -20%;
-        width: 70%;
-        border: none;
-        border-radius: 0;
-        font-size: 15px;
-        background: #212529b8;
+    /* Profile details styles */
+    .customer-name {
+        font-size: 24px;
+        font-weight: bold;
+        text-align: center;
+        margin-bottom: 10px;
+        color: #a50318;
     }
 
-    .profile-img .file input {
-        position: absolute;
-        opacity: 0;
-        right: 0;
-        top: 0;
+    .profile-info {
+        margin-top: 20px;
+        width: 100%;
     }
 
-    .profile-head h5 {
+    .info-item {
+        display: flex;
+        align-items: center;
+        margin-bottom: 10px;
+    }
+
+    .info-item label {
+        font-weight: bold;
+        width: 120px;
+        margin-right: 10px;
         color: #333;
     }
 
-    .profile-head h6 {
-        color: #0062cc;
+    .info-item p {
+        margin: 0;
+        word-wrap: break-word;
     }
 
-    .profile-edit-btn {
-        border: none;
-        border-radius: 1.5rem;
-        width: 70%;
-        font-weight: 600;
-        color: #360093;
-        cursor: pointer;
-        text-decoration: none;
+    /* Additional styling for info items */
+    .info-item .label-value {
+        display: flex;
+        align-items: center;
     }
 
-    .proile-rating {
-        font-size: 12px;
-        color: #818182;
-        margin-top: 5%;
+    .info-item .label-value:before {
+        content: ":";
+        margin-right: 5px;
+        color: #a50318;
     }
 
-    .proile-rating span {
-        color: #495057;
-        font-size: 15px;
-        font-weight: 600;
+    /* Responsive styles */
+    @media (max-width: 768px) {
+        .profile-card {
+            max-width: 400px;
+            margin: 20px;
+        }
+
+        .profile-img img {
+            width: 150px;
+            height: 150px;
+        }
+
+        .customer-name {
+            font-size: 20px;
+        }
+
+        .info-item label {
+            width: 100px;
+        }
     }
 
-    .profile-head .nav-tabs {
-        margin-bottom: 5%;
+    @media (max-width: 576px) {
+        .profile-img img {
+            width: 120px;
+            height: 120px;
+        }
+
+        .customer-name {
+            font-size: 18px;
+        }
+
+        .info-item label {
+            width: 90px;
+        }
     }
 
-    .profile-head .nav-tabs .nav-link {
-        font-weight: 600;
-        border: none;
-    }
+    @media (max-width: 375px) {
+        .profile-img img {
+            width: 100px;
+            height: 100px;
+        }
 
-    .profile-head .nav-tabs .nav-link.active {
-        border: none;
-        border-bottom: 2px solid #0062cc;
-    }
+        .customer-name {
+            font-size: 16px;
+        }
 
-    .profile-work {
-        padding: 14%;
-        margin-top: -15%;
-    }
-
-    .profile-work p {
-        font-size: 12px;
-        color: #818182;
-        font-weight: 600;
-        margin-top: 10%;
-    }
-
-    .profile-work a {
-        text-decoration: none;
-        color: #495057;
-        font-weight: 600;
-        font-size: 14px;
-    }
-
-    .profile-work ul {
-        list-style: none;
-    }
-
-    .profile-tab label {
-        font-weight: 600;
-    }
-
-    .profile-tab p {
-        font-weight: 600;
-        color: #0062cc;
+        .info-item label {
+            width: 80px;
+        }
     }
 </style>
 
-<body class="rowww">
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <div class="container emp-profile rowww">
-        <form method="post">
-            @foreach ($data as $dtata)
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="profile-img">
-                            <img src="{{$dtata->image}}" alt="" />
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="profile-head">
-                            <h5>
-
-                            </h5>
-                            <h6>
-                                <div class="customer h1 justify-content-center d-flex">
-                                    {{ $dtata->user_name }}
-                                </div>
-                            </h6>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-4">
-
-                    </div>
-                    <div class="col-md-8">
-                        <div class="tab-content profile-tab" id="myTabContent">
-                            <div class="tab-pane fade show active" id="home" role="tabpanel"
-                                aria-labelledby="home-tab">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Email</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>{{ $dtata->email }}</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Phone</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>{{ $dtata->contact }}</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Province</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>
-                                            @php
-                                                $province = DB::table('provinces')
-                                                    ->where('province_id', '=', $dtata->province)
-                                                    ->pluck('province')
-                                                    ->first();
-                                            @endphp
-                                            {{ $province }}</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>District</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        @php
-                                            $district = DB::table('districts')
-                                                ->where('district_id', '=', $dtata->district)
-                                                ->pluck('district')
-                                                ->first();
-                                        @endphp
-                                        <p>{{ $district }}</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Ward</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>{{ $dtata->ward }}</p>
-                                    </div>
-                                </div>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<div class="container emp-profile">
+    @foreach ($data as $dtata)
+        <div class="profile-card">
+            <div class="profile-img">
+                <img src="{{ $dtata->image }}" alt="Profile Image" />
+            </div>
+            <div class="profile-details">
+                <h5 class="customer-name" style="color: #a50318;">{{ $dtata->user_name }}</h5>
+                <div class="profile-info">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="info-item">
+                                <label>Email:</label>
+                                <p>{{ $dtata->email }}</p>
+                            </div>
+                            <div class="info-item">
+                                <label>Phone:</label>
+                                <p>{{ $dtata->contact }}</p>
+                            </div>
+                            <div class="info-item">
+                                <label>Province:</label>
+                                <p>{{ $dtata->province }}</p>
+                            </div>
+                            <div class="info-item">
+                                <label>District:</label>
+                                <p>{{ $dtata->district }}</p>
+                            </div>
+                            <div class="info-item">
+                                <label>Ward:</label>
+                                <p>{{ $dtata->ward }}</p>
                             </div>
                         </div>
                     </div>
                 </div>
-            @endforeach
-        </form>
-    </div>
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
-</body>
+            </div>
+        </div>
+    @endforeach
+</div>
 
-</html>
 @include('footermain')
