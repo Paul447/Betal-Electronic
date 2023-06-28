370142a2de5fc6ae982b8eb9659436671fd26472
Mukesh Mahato <mukeshmahato843@gmail.com>
2023-06-28 17:46:46 +0545

    fix: error when order is null in user profile view

--- app/Http/Controllers/viewprofileController.php
+++ app/Http/Controllers/viewprofileController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\admin\User;
 use App\Models\Order;
 use App\Models\deliverys;
@@ -9,41 +10,48 @@ use Illuminate\Http\Request;
 
 class viewprofileController extends Controller
 {
-    public function viewProfile(Request $request){
-        $data = session('customer','id');
+    public function viewProfile(Request $request)
+    {
+        $data = session('customer', 'id');
         $dataArray = json_decode($data, true);
         $id = $dataArray['id'];
-        $data = User::where('id','=' , $id)->get();
-       return view('customerview')->with(compact('data'));
+        $data = User::where('id', '=', $id)->get();
+        return view('customerview')->with(compact('data'));
     }
-    public function myorder(Request $request){
+    public function myorder(Request $request)
+    {
         $varr = $request->input('my');
-        $dataa = session('customer','id');
+        $dataa = session('customer', 'id');
         $dataArrayy = json_decode($dataa, true);
         $idd = $dataArrayy['id'];
         $mydata = Order::where('customer', $idd)
-        ->distinct()
-        ->pluck('created_at');
+            ->distinct()
+            ->pluck('created_at');
         // echo $mydata;
-        foreach($mydata as $data){
-         $requestData = deliverys::where('order_id','=',$data)->first();
-         $result[] = $requestData;
-        }   
-       return view('myorder')->with(compact('result'));
+        // dd($mydata);
+        foreach ($mydata as $data) {
+            $requestData = deliverys::where('order_id', '=', $data)->first();
+            $result[] = $requestData;
+        }
+        if (isset($result))
+            return view('myorder')->with(compact('result'));
+        else
+            return view('myorder');
     }
 
-    public function mydataView(Request $request){
+    public function mydataView(Request $request)
+    {
         $varr = $request->input('my');
         // echo $varr;
-        $orderProDet = Order::where('created_at','=',$varr)->get();
+        $orderProDet = Order::where('created_at', '=', $varr)->get();
         // echo $orderProDet;
-  
+
         // $hash = password_hash('hello', PASSWORD_BCRYPT);
         // echo $hash;
-    
-        
+
+
         // dd($orderProDet);
-        return view('myorderProductDetail')->with(compact('orderProDet'));   
-    //    return view('myorder')->with(compact('mydata'));
-   }
+        return view('myorderProductDetail')->with(compact('orderProDet'));
+        //    return view('myorder')->with(compact('mydata'));
+    }
 }
